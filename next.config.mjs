d97529083d2bae21363258406db44e0773cfb882
AnPQ <anpham1122000@gmail.com>
2026-01-14 23:14:33 +0700

    N1: fetch dish list

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api-bigboy.duthanhduoc.com',
+        port: '',
+        pathname: '/static/**',
+      },
+    ],
+  },
+}
 
 export default nextConfig
